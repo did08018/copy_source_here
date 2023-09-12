eef18aa827d20a21af989e74710d98d4cfcca47b
didier08018 <didier08018@gmail.com>
2023-09-12 14:10:57 -0400

    Commit One

---
+++ copy_write_simple/copy_write_ask.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+source = input("Enter the source directory: ")
+target = input("Enter the target directory: ")
+
+#print(source)
+#print(target)
+
+for root, dirs, files in os.walk(source):
+    for file in files:
+        path_file = os.path.join(root, file)
+        shutil.copy2(path_file, target)
